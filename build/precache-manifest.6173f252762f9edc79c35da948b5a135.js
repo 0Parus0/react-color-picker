e40b8e2492b24ba8e9867d2ae293e0fb776587c0
Parus <0Parus0@gmail.com>
2023-08-02 07:18:56 +0100

    removed build from .gitignore for hosting

---
+++ build/precache-manifest.6173f252762f9edc79c35da948b5a135.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70849a509926a2ff1706cc3c89ee6f07",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e6f340a09dc439aaccc5",
+    "url": "/static/css/2.753ea2eb.chunk.css"
+  },
+  {
+    "revision": "cd525d3543acc9955743",
+    "url": "/static/css/main.31c99a7f.chunk.css"
+  },
+  {
+    "revision": "e6f340a09dc439aaccc5",
+    "url": "/static/js/2.bcc7c4b3.chunk.js"
+  },
+  {
+    "revision": "45200e79f06de2111f9d159757776e6e",
+    "url": "/static/js/2.bcc7c4b3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cd525d3543acc9955743",
+    "url": "/static/js/main.f629b33a.chunk.js"
+  },
+  {
+    "revision": "2b28b9084bdd504f4836",
+    "url": "/static/js/runtime-main.3a210d15.js"
+  },
+  {
+    "revision": "6833ba5922f5a0050c1fd09e08081a81",
+    "url": "/static/media/bg.6833ba59.svg"
+  }
+]);
